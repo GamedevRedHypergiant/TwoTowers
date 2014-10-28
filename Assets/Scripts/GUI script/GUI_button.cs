63f1e62ac38163534a8a6d30d9f24cff4d2ef6f4
Jerionixs <andropof1>
2014-10-28 23:25:25 +0200

    pridejau mygtukus kad generuotu karius

---
+++ Assets/Scripts/GUI script/GUI_button.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_button : MonoBehaviour 
+{
+	
+	public GameObject enemy;
+	public Transform enemyPosition;
+
+	public GameObject friend;
+	public Texture box;
+	public Transform friendPosition;
+
+	private void OnGUI (){
+
+
+
+		//GUI.Box(new Rect(Screen.width -120, 0, Screen.width, Screen.height), new GUIContent(box));
+
+		if (GUI.Button (new Rect (50, 50, 100, 60), "Send Enemy")) {
+			Instantiate(enemy, enemyPosition.position, enemyPosition.rotation);	
+		}
+		 if (GUI.Button (new Rect (Screen.width - 150, Screen.height - 120, 100, 60), "Send Friend")) {
+			Instantiate(friend, friendPosition.position, friendPosition.rotation);	
+		}
+
+	}
+}
