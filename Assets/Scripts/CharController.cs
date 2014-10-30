72d2daddfa2731ddc79c38a38ee9d6d5b8ed5dae
Uzpykes <validolislt@gmail.com>
2014-10-30 16:22:21 +0200

    naujausi scripts

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -8,6 +8,7 @@ public class CharController : MonoBehaviour {
 	public Transform EnemyCastle;
 
 	public List<CharController> targets;
+	public CastleController enemyCastle;
 	
 
 	int countOfTargets = 0;
@@ -18,7 +19,11 @@ public class CharController : MonoBehaviour {
 	
 	Vector3 enemyPos;
 	bool hasTarget = false;
+	bool hasTargetCastle = false;
 	bool targetReached = false;
+	bool taskDone = false;
+	public bool cantMove = false;
+	public bool shouldIFly = false;
 
 	CharacterController characterController;
 
@@ -31,12 +36,24 @@ public class CharController : MonoBehaviour {
 	public float AttackSpeed = 0.7f;
 	public float Damage = 5f;
 
+	public Transform goldStorage;
+
+	float goldGivenOnDeath = 5f;
+
+	public Vector3 originalPos;
+
+
 
 	// Use this for initialization
 	void Start () {
 		targets = new List<CharController> ();
 		seeker = GetComponent<Seeker> ();
 		seeker.StartPath (transform.position, EnemyCastle.position, OnPathComplete);
+		if (gameObject.tag == "GoodGuy") {
+						goldStorage = GameObject.FindGameObjectWithTag ("BadCastle").transform;
+				} else if (gameObject.tag == "Enemy") {
+						goldStorage = GameObject.FindGameObjectWithTag ("GoodCastle").transform;
+				}
 	//	characterController = GetComponent<CharacterController> ();
 	}
 
@@ -54,12 +71,21 @@ public class CharController : MonoBehaviour {
 			targets.Add(o.GetComponent<CharController>());
 			hasTarget = true;
 			countOfTargets++;
+		} else if (((gameObject.tag == "GoodGuy") && (o.tag == "BadCastle")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodCastle"))) {
+			enemyCastle = o.GetComponent<CastleController>();
+			hasTargetCastle = true;
 		}
 	}
 
 	void OnTriggerExit(Collider o) {
 		if ((((gameObject.tag == "GoodGuy") && (o.tag == "Enemy")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodGuy"))) && countOfTargets == 0) {
-		//	hasTarget = false;
+			hasTarget = false;
+		} 
+
+		if (((gameObject.tag == "GoodGuy") && (o.tag == "BadCastle")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodCastle"))) {
+			enemyCastle = o.GetComponent<CastleController>();
+			hasTargetCastle = false;
+			targetReached = false;			
 		}
 	}
 
@@ -74,7 +100,7 @@ public class CharController : MonoBehaviour {
 		
 				Vector3 dir = (path.vectorPath [currentWaypoint] - transform.position);
 				//characterController.SimpleMove (dir);
-		transform.Translate (Vector3.forward * Time.deltaTime * Speed);
+				transform.Translate (Vector3.forward * Time.deltaTime * Speed);
 				transform.LookAt(transform.position + dir);
 				
 				if (Vector3.Distance (transform.position, path.vectorPath [currentWaypoint]) < 10f) {
@@ -86,31 +112,50 @@ public class CharController : MonoBehaviour {
 		}
 
 	void getToTarget() {
-						transform.LookAt (targets [0].transform.position);
-						if (Vector3.Distance (transform.position, targets[0].transform.position) > 10f) {
-								transform.Translate (Vector3.forward * Time.deltaTime * Speed);
-						} else { 
-								targetReached = true;
+				if (targets.Count > 0) {
+						if ((targets [0] != null) && (hasTarget)) {
+								transform.LookAt (targets [0].transform.position);
+								if (Vector3.Distance (transform.position, targets [0].transform.position) > 10f) {
+										transform.Translate (Vector3.forward * Time.deltaTime * Speed);
+								} else { 
+										targetReached = true;
+								}
+						} else {
+								hasTarget = false;
+								targets.RemoveAt (0);
+								countOfTargets--;
 						}
+				} else if (hasTargetCastle) {
+			transform.LookAt (enemyCastle.transform.position);
+			if (Vector3.Distance (transform.position, enemyCastle.transform.position) > 45f) {
+				transform.Translate (Vector3.forward * Time.deltaTime * Speed);
+			} else { 
+				targetReached = true;
+			}
 				}
 
+		}
+	
+
 	void attack() {
+
 		nextAttackTime = Time.time + AttackSpeed;
 
 		bool isBlocked = false;
-		if (Random.Range (0, 99) < ChanceToBlock) {
+		if (Random.Range (0, 99) < targets[0].ChanceToBlock) {
 						isBlocked = true;
 				}
 
-		if(targets[0].Hitpoints <= 0) {
-			targets.Remove (targets[0]);
-			countOfTargets--;
-			if(countOfTargets <= 0) {
-				hasTarget = false;
-			}
-		} else if (!isBlocked) {
-			targets [0].Hitpoints -= Damage;
-		}
+				if (targets [0].Hitpoints <= 0) {
+						targets.Remove (targets [0]);
+						countOfTargets--;
+						if (countOfTargets <= 0) {
+								hasTarget = false;
+								targetReached = false;
+						}
+				} else if (!isBlocked) {
+						targets [0].Hitpoints -= Damage;
+				} 
 
 
 						
@@ -118,28 +163,68 @@ public class CharController : MonoBehaviour {
 		animation.Play("attack");
 	}
 
+	void attackCastle() {
+
+				if (enemyCastle.Hitpoints > 0) {
+						nextAttackTime = Time.time + AttackSpeed;
+						enemyCastle.Hitpoints -= Damage;	
+
+						animation ["attack"].speed = 0.7f;
+						animation.Play ("attack");
+				} else {
+					hasTargetCastle = false;
+					taskDone = true;
+				}			
+		}
+
+	void giveGold() {
+		goldStorage.GetComponent<CastleController>().gold += goldGivenOnDeath;
+	}
 
 	void die() {
+		giveGold ();
 		Destroy (gameObject);
+	}
+
+	void idle() {
+		animation.Play ("idle");
+		}
+
+	void cachExplosion() {
+		transform.position -= transform.forward * Time.deltaTime * 100;
+		if (Vector3.Distance (transform.position, originalPos) > 50f) {
+						shouldIFly = false;
+						cantMove = false;
+				}
 	}
 	
 
 	// Update is called once per frame
 	void Update () {
-				
-				if (!hasTarget) {
-						FollowPath ();
-						Debug.Log ("einam");
-						
-				} else 	if (targetReached) {
-						if (Time.time >= nextAttackTime) {
-								attack ();
+		transform.position = transform.position + Vector3.zero;
+		if (!taskDone && !cantMove) {
+						if (!hasTarget && !hasTargetCastle) {
+								FollowPath ();
+						} else 	if (targetReached) {
+
+								if (Time.time >= nextAttackTime) {
+										if (hasTarget) {
+												attack ();
+										} else if (hasTargetCastle) {
+												attackCastle ();
+										}	
+								}
+						} else if (hasTarget || hasTargetCastle) {
+								getToTarget ();
 						}
+				} else if (shouldIFly) {
+					cachExplosion();
+				} else {
+						idle ();
 				}
-				else if (hasTarget) {
-						getToTarget ();
-						Debug.Log ("to target");
-				} 
+
+
+
 				if (Hitpoints <= 0) {
 						die ();
 				}
