4da595fa6f009068c3d6fd6f984018c77c35b575
Jerionixs <andropof1>
2014-11-16 00:23:40 +0200

    fixed start and exit buttons, Sound on off

---
+++ Assets/Scripts/MusicOnOff.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MusicOnOff : MonoBehaviour {
+
+	public Toggle toggle;
+
+	public void soundChange()
+	{
+		if (toggle.isOn) 
+		{
+			Debug.Log("on");
+			AudioListener.pause = false;
+		}
+		else
+		{
+			Debug.Log("off");
+			AudioListener.pause = true;
+		}
+	}
+}
