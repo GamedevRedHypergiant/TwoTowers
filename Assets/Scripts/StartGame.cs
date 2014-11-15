befa144a659f725aac12d296d92beb3c559b1d2d
Jerionixs <andropof1>
2014-11-15 22:45:43 +0200

    Main meniu (start and exit buttons)

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+
+
+	public void startLevel(int sceneIndex)
+	{
+		Application.LoadLevel(sceneIndex);
+	}
+}
