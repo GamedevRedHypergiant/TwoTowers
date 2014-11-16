31e9cb0eaeac8ecd04243bd78b9e675333105ce7
Jerionixs <andropof1>
2014-11-16 23:07:41 +0200

    Purchasing soldiers(active,inactive button), Player script for collecting info about purchased soldiers

---
+++ Assets/PlayerEntities.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEntities : MonoBehaviour {
+
+	private int Warriors;
+	private int Mages;
+	
+	// Use this for initialization
+	void Start () {
+		Warriors = 0;
+		Mages = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void incWarriors()
+	{
+		++Warriors;
+		print ("eina!!!");
+	}
+
+	public int getWarriors()
+	{
+		return Warriors;
+	}
+
+
+
+}
