c08e2d7ba3f76a787c005947dc80937c843edad9
Jerionixs <andropof1>
2014-11-17 19:48:58 +0200

    Fixed send army sliders

--- Assets/Scripts/GUI script/GUISendArmy.cs
+++ Assets/Scripts/GUI script/GUISendArmy.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GUISendArmy : MonoBehaviour {
+
+	public Text warrior_send_count_text;
+	public Text mage_send_count_text;
+
+	public Slider slider_warrior;
+	public Slider slider_mage;
 
 	public GameObject warrior;
 	public Transform warrior_position;
@@ -15,26 +22,29 @@ public class GUISendArmy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		warrior_send_count_text.text = slider_warrior.value.ToString ();
+		mage_send_count_text.text = slider_mage.value.ToString ();
+		slider_mage.maxValue = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().getMages();
+		slider_warrior.maxValue = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().getWarriors();
 	}
 
 	public void sendArmy ()
 	{
-		int j = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().getWarriors ();
+		int j = (int) slider_warrior.value;
 		for (int i=0; i<j; ++i)
 		{
 			GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().decWarriors ();
 			Instantiate (warrior, warrior_position.position, warrior_position.rotation);
-
 		}
 
-		j = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().getMages ();
+		j = (int) slider_mage.value;
 		for (int i=0; i<j; ++i)
 		{
 			GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().decMages ();
 			Instantiate (mage, mage_position.position, mage_position.rotation);
-
 		}
 
 	}
+
+
 }
