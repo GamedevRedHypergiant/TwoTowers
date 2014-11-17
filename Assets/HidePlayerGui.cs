de326eb4c72a29a5c3ab365cf78c38a3ae0b5938
Jerionixs <andropof1>
2014-11-17 21:37:21 +0200

    show and hide GUI

---
+++ Assets/HidePlayerGui.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HidePlayerGui : MonoBehaviour {
+
+	public GameObject playerGUI;
+
+	void OnMouseDown() {
+		playerGUI.SetActive (false);
+	}
+}
---
+++ Assets/ShowPlayerGui.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPlayerGui : MonoBehaviour {
+
+	public GameObject playerGUI;
+
+	void OnMouseDown() {
+		playerGUI.SetActive (true);
+	}
+
+	// Use this for initialization
+	void Start () {
+		playerGUI.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
