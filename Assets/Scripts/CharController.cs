b4f5528ad406bffbc2bb6521cf61a33d76c8d50f
Jerionixs <andropof1>
2014-11-29 01:29:21 +0200

    GUI show info when buying, fixed troll animation, edited damage(depends on soldier type)

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -3,254 +3,333 @@ using System.Collections;
 using System.Collections.Generic;
 using Pathfinding;
 
-public class CharController : MonoBehaviour {
+public class CharController : MonoBehaviour
+{
+    //Koks karys( mage:1, goodGuy:2, Troll:3)
+    public int type;
+    private Transform EnemyCastle;
+    private List<CharController> Targets;
+    private CastleController enemyCastleController;
+    private int countOfTargets = 0;
+    Seeker seeker;
+    Path path;
+    int currentWaypoint;
+    Vector3 enemyPos;
+    bool hasTarget = false;
+    bool hasTargetCastle = false;
+    bool targetReached = false;
+    bool taskDone = false;
+    public bool cantMove = false;
+    public bool shouldIFly = false;
+    public bool attacksFromRange;
+    public Transform projectile;
+    CharacterController characterController;
+    float nextAttackTime;
+    
+    //character gameplay values
+    public float damageToTroll;
+    public float damageToMage;
+    public float damageToWarrior;
+    public float damageToTower;
+    public float Speed;
+    public float Hitpoints = 100f;
+    public float ChanceToBlock = 5f;
+    public float AttackSpeed = 0.7f;
+    public float price = 10f;
+    private Transform goldStorage;
+    float goldGivenOnDeath = 5f;
+    public Vector3 originalPos;
+    private bool SetTimeTroll = true;
 
-	public Transform EnemyCastle;
+    // Use this for initialization
+    void Start()
+    {
+        Targets = new List<CharController>();
+        if (gameObject.tag == "GoodGuy")
+        {
+            EnemyCastle = GameObject.FindGameObjectWithTag("BadCastle").transform;
+        } else if (gameObject.tag == "Enemy")
+        {
+            EnemyCastle = GameObject.FindGameObjectWithTag("GoodCastle").transform;
+        }
+        seeker = GetComponent<Seeker>();
+        seeker.StartPath(transform.position, EnemyCastle.position, OnPathComplete);
+        if (gameObject.tag == "GoodGuy")
+        {
+            goldStorage = GameObject.FindGameObjectWithTag("BadCastle").transform;
+        } else if (gameObject.tag == "Enemy")
+        {
+            goldStorage = GameObject.FindGameObjectWithTag("GoodCastle").transform;
+        }
+        //  characterController = GetComponent<CharacterController> ();
+    }
 
-	public List<CharController> targets;
-	public CastleController enemyCastle;
-	
+    public void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        } else
+        {
+            Debug.Log(p.error);
+        }
+    }
 
-	int countOfTargets = 0;
+    void OnTriggerEnter(Collider o)
+    {
+        if (((gameObject.tag == "GoodGuy") && (o.tag == "Enemy")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodGuy")))
+        {
+            Targets.Add(o.GetComponent<CharController>());
+            hasTarget = true;
+            if (attacksFromRange)
+            {
+                targetReached = true;
+            }
+            countOfTargets++;
+        } else if (((gameObject.tag == "GoodGuy") && (o.tag == "BadCastle")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodCastle")))
+        {
+            enemyCastleController = o.GetComponent<CastleController>();
+            hasTargetCastle = true;
+            if (attacksFromRange)
+            {
+                targetReached = true;
+            }
+        }
+    }
 
-	Seeker seeker;
-	Path path;
-	int currentWaypoint;
-	
-	Vector3 enemyPos;
-	bool hasTarget = false;
-	bool hasTargetCastle = false;
-	bool targetReached = false;
-	bool taskDone = false;
-	public bool cantMove = false;
-	public bool shouldIFly = false;
-	public bool attacksFromRange;
-	public Transform projectile;
+    void OnTriggerExit(Collider o)
+    {
+        if ((((gameObject.tag == "GoodGuy") && (o.tag == "Enemy")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodGuy"))) && countOfTargets == 0)
+        {
+            hasTarget = false;
+        } 
 
-	CharacterController characterController;
+        if (((gameObject.tag == "GoodGuy") && (o.tag == "BadCastle")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodCastle")))
+        {
+            enemyCastleController = o.GetComponent<CastleController>();
+            hasTargetCastle = false;
+            targetReached = false;          
+        }
+    }
 
-	float nextAttackTime;
-	
-	//character gameplay values
-	public float Speed;
-	public float Hitpoints = 100f;
-	public float ChanceToBlock = 5f;
-	public float AttackSpeed = 0.7f;
-	public float Damage = 5f;
-	public float price = 10f;
+    void FollowPath()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+        
+        Vector3 dir = (path.vectorPath [currentWaypoint] - transform.position);
+        //characterController.SimpleMove (dir);
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        transform.LookAt(transform.position + dir);
+                
+        if (Vector3.Distance(transform.position, path.vectorPath [currentWaypoint]) < 10f)
+        {
+            currentWaypoint++;
+                        
+        }
+        animation ["run"].speed = 0.7f;
+        animation.Play("run");
+    }
 
-	public Transform goldStorage;
+    void getToTarget()
+    {
+        if (Targets.Count > 0)
+        {
+            if ((Targets [0] != null) && (hasTarget))
+            {
+                transform.LookAt(Targets [0].transform.position);
+                if (Vector3.Distance(transform.position, Targets [0].transform.position) > 10f)
+                {
+                    transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+                } else
+                { 
+                    targetReached = true;
+                }
+            } else
+            {
+                hasTarget = false;
+                Targets.RemoveAt(0);
+                countOfTargets--;
+            }
+        } else if (hasTargetCastle)
+        {
+            transform.LookAt(enemyCastleController.transform.position);
+            if (Vector3.Distance(transform.position, enemyCastleController.transform.position) > 45f)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+            } else
+            { 
+                targetReached = true;
+            }
+        }
 
-	float goldGivenOnDeath = 5f;
+    }
 
-	public Vector3 originalPos;
+    void attack()
+    {
+        if (type == 3)
+        {
+            nextAttackTime = Time.time + (AttackSpeed*2/3);
+            SetTimeTroll = true;
+        } else
+        {
+            nextAttackTime = Time.time + AttackSpeed;
+        }
 
+        bool isBlocked = false;
+        if (Random.Range(0, 99) < Targets [0].ChanceToBlock)
+        {
+            isBlocked = true;
+        }
 
+        if (!isBlocked)
+        {
+            
+            if (Vector3.Distance(transform.position, Targets [0].transform.position) <= 10f)
+            {
+                targetReached = false;
+            }
+                        
+            if (Targets [0].type == 1)
+            {
+                Targets [0].Hitpoints -= damageToMage;
+            } else if (Targets [0].type == 2)
+            {
+                Targets [0].Hitpoints -= damageToWarrior;
+            } else
+            {
+                Targets [0].Hitpoints -= damageToTroll;
+            }
 
-	// Use this for initialization
-	void Start () {
-		targets = new List<CharController> ();
-		if (gameObject.tag == "GoodGuy") {
-			EnemyCastle = GameObject.FindGameObjectWithTag ("BadCastle").transform;
-		} else if (gameObject.tag == "Enemy") {
-			EnemyCastle = GameObject.FindGameObjectWithTag ("GoodCastle").transform;
-		}
-		seeker = GetComponent<Seeker> ();
-		seeker.StartPath (transform.position, EnemyCastle.position, OnPathComplete);
-		if (gameObject.tag == "GoodGuy") {
-						goldStorage = GameObject.FindGameObjectWithTag ("BadCastle").transform;
-				} else if (gameObject.tag == "Enemy") {
-						goldStorage = GameObject.FindGameObjectWithTag ("GoodCastle").transform;
-				}
-	//	characterController = GetComponent<CharacterController> ();
-	}
+            if (projectile != null)
+            {
+                Instantiate(projectile, Targets [0].transform.position, Targets [0].transform.rotation);
+            }
+        }
 
-	public void OnPathComplete(Path p) {
-		if (!p.error) {
-						path = p;
-						currentWaypoint = 0;
-				} else {
-						Debug.Log (p.error);
-				}
-	}
+        if (Targets [0].Hitpoints <= 0)
+        {
+            Targets.Remove(Targets [0]);
+            countOfTargets--;
+            if (countOfTargets <= 0)
+            {
+                hasTarget = false;
+                targetReached = false;
+            }
+        }
 
-	void OnTriggerEnter (Collider o) {
-		if (((gameObject.tag == "GoodGuy") && (o.tag == "Enemy")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodGuy"))) {
-			targets.Add(o.GetComponent<CharController>());
-			hasTarget = true;
-			if (attacksFromRange) {
-				targetReached = true;
-			}
-			countOfTargets++;
-		} else if (((gameObject.tag == "GoodGuy") && (o.tag == "BadCastle")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodCastle"))) {
-			enemyCastle = o.GetComponent<CastleController>();
-			hasTargetCastle = true;
-			if (attacksFromRange) {
-				targetReached = true;
-			}
-		}
-	}
+        //animation for models except troll
+        if (type != 3)
+        {
+            //animation ["attack"].speed = 0.6f;        
+            animation ["attack"].speed = 0.7f;
+            animation.Play("attack");
+        }
+                
+    }
 
-	void OnTriggerExit(Collider o) {
-		if ((((gameObject.tag == "GoodGuy") && (o.tag == "Enemy")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodGuy"))) && countOfTargets == 0) {
-			hasTarget = false;
-		} 
+    void attackCastle()
+    {
 
-		if (((gameObject.tag == "GoodGuy") && (o.tag == "BadCastle")) || ((gameObject.tag == "Enemy") && (o.tag == "GoodCastle"))) {
-			enemyCastle = o.GetComponent<CastleController>();
-			hasTargetCastle = false;
-			targetReached = false;			
-		}
-	}
+        if (enemyCastleController.Hitpoints > 0)
+        {
+            nextAttackTime = Time.time + AttackSpeed;
+            enemyCastleController.Hitpoints -= damageToTower;   
 
-	void FollowPath() {
-				if (path == null) {
-						return;
-				}
-		
-				if (currentWaypoint >= path.vectorPath.Count) {
-						return;
-				}
-		
-				Vector3 dir = (path.vectorPath [currentWaypoint] - transform.position);
-				//characterController.SimpleMove (dir);
-				transform.Translate (Vector3.forward * Time.deltaTime * Speed);
-				transform.LookAt(transform.position + dir);
-				
-				if (Vector3.Distance (transform.position, path.vectorPath [currentWaypoint]) < 10f) {
-						currentWaypoint++;
-						
-				}
-		animation ["run"].speed = 0.7f;
-				animation.Play("run");
-		}
+            animation ["attack"].speed = 0.7f;
+            animation.Play("attack");
+        } else
+        {
+            hasTargetCastle = false;
+            taskDone = true;
+        }           
+    }
 
-	void getToTarget() {
-				if (targets.Count > 0) {
-						if ((targets [0] != null) && (hasTarget)) {
-								transform.LookAt (targets [0].transform.position);
-								if (Vector3.Distance (transform.position, targets [0].transform.position) > 10f) {
-										transform.Translate (Vector3.forward * Time.deltaTime * Speed);
-								} else { 
-										targetReached = true;
-								}
-						} else {
-								hasTarget = false;
-								targets.RemoveAt (0);
-								countOfTargets--;
-						}
-				} else if (hasTargetCastle) {
-			transform.LookAt (enemyCastle.transform.position);
-			if (Vector3.Distance (transform.position, enemyCastle.transform.position) > 45f) {
-				transform.Translate (Vector3.forward * Time.deltaTime * Speed);
-			} else { 
-				targetReached = true;
-			}
-				}
+    void giveGold()
+    {
+        goldStorage.GetComponent<CastleController>().gold += goldGivenOnDeath;
+    }
 
-		}
-	
+    void die()
+    {
+        giveGold();
+        Destroy(gameObject);
+    }
 
-	void attack() {
+    void idle()
+    {
+        animation.Play("idle");
+    }
 
-		nextAttackTime = Time.time + AttackSpeed;
+    void cachExplosion()
+    {
+        transform.position -= transform.forward * Time.deltaTime * 100;
+        if (Vector3.Distance(transform.position, originalPos) > 50f)
+        {
+            shouldIFly = false;
+            cantMove = false;
+        }
+    }
+    
 
-		bool isBlocked = false;
-		if (Random.Range (0, 99) < targets[0].ChanceToBlock) {
-						isBlocked = true;
-				}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + Vector3.zero;
+        if (!taskDone && !cantMove)
+        {
+            if (!hasTarget && !hasTargetCastle)
+            {
+                FollowPath();
+            } else  if (targetReached)
+            {
 
+                if (Time.time >= nextAttackTime)
+                {                                      
+                                        
+                    if (hasTarget)
+                    {
+                            if (SetTimeTroll && type == 3)
+                            {
+                                //animation ["attack"].speed = 0.6f;        
+                                animation.Play("attack");
+                                SetTimeTroll = false;
+                                //kad uzduodant smugi veiktu animcja anksciau
+                                nextAttackTime = Time.time + (AttackSpeed/3);
+                            }else
+                            attack();
 
-				if (targets [0].Hitpoints <= 0) {
-						targets.Remove (targets [0]);
-						countOfTargets--;
-						if (countOfTargets <= 0) {
-								hasTarget = false;
-								targetReached = false;
-						}
-				} else if (!isBlocked) {
-			
-						if (Vector3.Distance (transform.position, targets [0].transform.position) <= 10f) {
-							targetReached = false;
-						}
-
-						targets [0].Hitpoints -= Damage;
-							if (projectile != null) {
-								Instantiate(projectile, targets[0].transform.position, targets[0].transform.rotation);
-							}
-				} 
-
-
-						
-		animation ["attack"].speed = 0.7f;
-		animation.Play("attack");
-	}
-
-	void attackCastle() {
-
-				if (enemyCastle.Hitpoints > 0) {
-						nextAttackTime = Time.time + AttackSpeed;
-						enemyCastle.Hitpoints -= Damage;	
-
-						animation ["attack"].speed = 0.7f;
-						animation.Play ("attack");
-				} else {
-					hasTargetCastle = false;
-					taskDone = true;
-				}			
-		}
-
-	void giveGold() {
-		goldStorage.GetComponent<CastleController>().gold += goldGivenOnDeath;
-	}
-
-	void die() {
-		giveGold ();
-		Destroy (gameObject);
-	}
-
-	void idle() {
-		animation.Play ("idle");
-		}
-
-	void cachExplosion() {
-		transform.position -= transform.forward * Time.deltaTime * 100;
-		if (Vector3.Distance (transform.position, originalPos) > 50f) {
-						shouldIFly = false;
-						cantMove = false;
-				}
-	}
-	
-
-	// Update is called once per frame
-	void Update () {
-		transform.position = transform.position + Vector3.zero;
-		if (!taskDone && !cantMove) {
-						if (!hasTarget && !hasTargetCastle) {
-								FollowPath ();
-						} else 	if (targetReached) {
-
-								if (Time.time >= nextAttackTime) {
-										if (hasTarget) {
-												attack ();
-										} else if (hasTargetCastle) {
-												attackCastle ();
-										}	
-								}
-						} else if (hasTarget || hasTargetCastle) {
-								getToTarget ();
-						}
-				} else if (shouldIFly) {
-					cachExplosion();
-				} else {
-						idle ();
-				}
+                    } else if (hasTargetCastle)
+                    {
+                        attackCastle();
+                    }   
+                }
+            } else if (hasTarget || hasTargetCastle)
+            {
+                getToTarget();
+            }
+        } else if (shouldIFly)
+        {
+            cachExplosion();
+        } else
+        {
+            idle();
+        }
 
 
 
-				if (Hitpoints <= 0) {
-						die ();
-				}
-			}		
-		}
+        if (Hitpoints <= 0)
+        {
+            die();
+        }
+    }       
+}
 
--- Assets/Scripts/GUI script/ButtonBehaviour.cs
+++ Assets/Scripts/GUI script/ButtonBehaviour.cs
@@ -12,6 +12,11 @@ public class ButtonBehaviour : MonoBehaviour {
 	public GameObject magic_object;
 	public GameObject troll_object;
 	
+    public GameObject attributePanel;
+    public Text soldierName;
+    public Text soldierInfoDamage;
+    public Text soldierInfoElse;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -22,6 +27,7 @@ public class ButtonBehaviour : MonoBehaviour {
 		ButtonEnableWarriors ();
 		ButtonEnableMages ();
 		ButtonEnableTrolls ();
+
 	}
 
 	private void ButtonEnableWarriors()
@@ -115,4 +121,50 @@ public class ButtonBehaviour : MonoBehaviour {
 			GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerEntities> ().incTrolls();
 		}
 	}
+
+    public void onMouseEnter(Button buttonEnter)
+    {
+        string temp;
+        if (buttonEnter == warrior_button)
+        {
+            soldierName.text = "Warrior";
+            temp = "Damage:\nWarrior: " + warrior_object.GetComponent<CharController>().damageToWarrior.ToString();
+            temp += "\nMage: " + warrior_object.GetComponent<CharController>().damageToMage.ToString();
+            temp += "\nTroll: " + warrior_object.GetComponent<CharController>().damageToTroll.ToString();
+            soldierInfoDamage.text = temp;
+            temp = "Normal attack\n";
+            temp += "Life: " + warrior_object.GetComponent<CharController>().Hitpoints.ToString();
+            soldierInfoElse.text = temp;
+        }
+        if (buttonEnter == magic_button)
+        {
+            soldierName.text = "Mage";
+            temp = "Damage:\nWarrior: " + magic_object.GetComponent<CharController>().damageToWarrior.ToString();
+            temp += "\nMage: " + magic_object.GetComponent<CharController>().damageToMage.ToString();
+            temp += "\nTroll: " + magic_object.GetComponent<CharController>().damageToTroll.ToString();
+            soldierInfoDamage.text = temp;
+            temp = "Spell attack\n";
+            temp += "Life: " + magic_object.GetComponent<CharController>().Hitpoints.ToString();
+            soldierInfoElse.text = temp;
+        }
+        if (buttonEnter == troll_button)
+        {
+            soldierName.text = "Troll";
+            temp = "Damage:\nWarrior: " + troll_object.GetComponent<CharController>().damageToWarrior.ToString();
+            temp += "\nMage: " + troll_object.GetComponent<CharController>().damageToMage.ToString();
+            temp += "\nTroll: " + troll_object.GetComponent<CharController>().damageToTroll.ToString();
+            soldierInfoDamage.text = temp;
+            temp = "Area attack\n";
+            temp += "Life: " + troll_object.GetComponent<CharController>().Hitpoints.ToString();
+            soldierInfoElse.text = temp;
+        }
+        attributePanel.SetActive(true);
+    }
+    
+    public void onMouseExit()
+    {
+        attributePanel.SetActive(false);
+    }
+
+
 }
