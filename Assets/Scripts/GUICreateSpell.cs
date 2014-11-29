84bed2ec3a112cd892faecfad7fa37da610b0825
Uzpykes <validolislt@gmail.com>
2014-11-29 20:24:15 +0200

    Pridetas AOE spell selector

---
+++ Assets/Scripts/GUICreateSpell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUICreateSpell : MonoBehaviour {
+
+	public GameObject AOE;
+
+	// Use this for initialization
+	public void SpawnSpell() {
+		Instantiate (AOE);
+	}
+
+
+}
---
+++ Assets/Scripts/MagicSpellController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class MagicSpellController : MonoBehaviour 
+{
+	
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	float distanceToGround;
+	RaycastHit hit;
+	Ray ray;
+	int layerMask = 1 << 11;
+
+	void Start() {
+		transform.Rotate (Vector3.back);
+	}
+
+	void Update() {
+		// prepare raycast
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, 1000, layerMask))
+		{
+			if (hit.transform.tag == "Terrain") {
+				transform.position = hit.point ;
+			}
+		}
+	}
+
+
+
+
+}
