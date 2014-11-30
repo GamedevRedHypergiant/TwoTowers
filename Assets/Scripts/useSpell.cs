828eb6dbb100d57400e35c60bb7851e43da32294
Alex <sashaa355@gmail.com>
2014-11-30 17:59:47 +0200

    added spell

---
+++ Assets/Scripts/useSpell.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class useSpell : MonoBehaviour {
+	
+	public rotate spellController;
+	public Text cd;
+	private float time = 0;
+	
+	void Update(){
+		if (time > 0) {
+			time -= Time.deltaTime;
+			cd.text = "Cooldown :" + (int)time;
+		}
+		else {
+			cd.text = "Ready!";
+		}
+	}
+	
+	public void useFireWall(){
+		if (time <= 0) {
+			spellController.on = true;
+			spellController.proj.enabled = true;
+			time = 60;
+		}
+	}
+	
+}
---
+++ Assets/spell/rotate.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public ParticleSystem effect;
+	public Projector proj;
+	public float speed;
+	public bool on;
+	private float time = 0;
+
+	// Use this for initialization
+	void Start () {
+		//Instantiate (effect);
+		Instantiate(effect);
+		effect.enableEmission = false;
+		collider.enabled = false;
+		proj.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (on) {
+			if (collider.enabled) {
+				time += Time.deltaTime;
+				if (time >= 2) {
+					collider.enabled = false;
+					//Destroy (effect);
+					time = 0;
+					on = false;
+				}
+			} else {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+
+				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+					Vector3 currentMousePos = new Vector3 (hit.point.x, hit.point.y, hit.point.z);
+					transform.position = new Vector3 (currentMousePos.x, 600, currentMousePos.z);
+					if (Input.GetMouseButtonDown (0)) {
+						Debug.Log (effect.transform.position.x);
+						effect.transform.position = new Vector3 (currentMousePos.x, currentMousePos.y + 50, currentMousePos.z);
+						effect.Play();
+						proj.enabled = false;
+						collider.enabled = true;
+						Debug.Log ("perjungiau");
+					}
+				}
+				transform.Rotate (0, 0, speed);
+			}
+		}
+	}
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Enemy")
+			other.GetComponent<CharController> ().Hitpoints -= 50;
+		Debug.Log (other.tag);
+	}
+}
