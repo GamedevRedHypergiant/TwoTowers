fd818af21eaf6c647322df671f02cafb82f4bca1
Uzpykes <validolislt@gmail.com>
2014-12-01 10:30:59 +0200

    Main meniu nuoroda

---
+++ Assets/Scripts/loadMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void loadMain() {
+		Application.LoadLevel(0);
+	}
+}
